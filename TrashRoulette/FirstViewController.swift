3a0ca1e0c52c46bad5e0d44030540f3565d42723
buitim <buitim@oregonstate.edu>
2018-12-15 04:16:41 -0800

    search by id works... fuck pagination

--- TrashRoulette/FirstViewController.swift
+++ TrashRoulette/FirstViewController.swift
@@ -12,17 +12,29 @@ class FirstViewController: UIViewController {
 	@IBOutlet var showTitle: UILabel!
 	@IBOutlet var showID: UILabel!
 	@IBOutlet weak var showArt: UIImageView!
+	@IBOutlet weak var searchField: UITextField!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		apollo.fetch(query: GetShowQuery(showID: 101165)) { result, _ in
+		
+	}
+	
+	
+	@IBAction func runSearch(_ sender: Any) {
+		print("== Text Box: \(searchField.text!)")
+		runQuery(showID: Int(searchField.text!)!)
+	}
+	
+	func runQuery(showID: Int) {
+		apollo.fetch(query: GetShowQuery(showID: showID)) { result, _ in
 			guard let data = result?.data else { return }
-			self.showID.text = "\((data.media?.id)!)" // Force unwrap because swift is a bitch
+			self.showID.text = "\((data.media?.title?.native)!)" // Force unwrap because swift is a bitch
 			self.showTitle.text = data.media?.title?.romaji
-			let imageURL = URL(string: (data.media?.coverImage?.large)!)
+			let imageURL = URL(string: (data.media?.coverImage?.extraLarge)!)
 			self.showArt.setImage(url: imageURL!)
 		}
 	}
 }
+
