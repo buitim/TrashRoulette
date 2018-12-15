0d290b73f55d8e58de4ecb88a42c6990dc6275e4
buitim <buitim@oregonstate.edu>
2018-12-14 21:54:57 -0800

    Init

---
+++ TrashRoulette/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TrashRoulette
+//
+//  Created by Timothy Bui on 12/14/18.
+//  Copyright © 2018 Timothy Bui. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
